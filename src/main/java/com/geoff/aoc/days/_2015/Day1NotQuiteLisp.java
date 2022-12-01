759d108ebae3c38f282dcb31c288e021f827d9d7
Geoff Carlson <carlson.geoff@gmail.com>
2022-11-30 21:48:33 -0500

    2015 day 1 part 2

--- src/main/java/com/geoff/aoc/days/_2015/Day1NotQuiteLisp.java
+++ src/main/java/com/geoff/aoc/days/_2015/Day1NotQuiteLisp.java
@@ -7,16 +7,20 @@ import com.geoff.aoc.util.PuzzleInput;
 
 @Component
 public class Day1NotQuiteLisp implements AoCDay {
+    String input;
+
+    public Day1NotQuiteLisp() {
+        input = PuzzleInput.readPuzzleInput(2015, 1);
+    }
 
     @Override
     public String part1() {
-        String input = PuzzleInput.readPuzzleInput(2015, 1);
         return Integer.toString(findFloor(input));
     }
 
     @Override
     public String part2() {
-        return "not yet";
+        return Integer.toString(findBasement(input));
     }
     
     public int findFloor(String instructions) {
@@ -31,13 +35,38 @@ public class Day1NotQuiteLisp implements AoCDay {
                 case ')':
                     floor--;
                     break;
-                default:
-                    System.out.println("Error in Day1NotQuiteLisp.findFloor");
             }
         }
 
         System.out.println("Day1NotQuiteLisp.findFloor: " + floor);
 
         return floor;
+    }
+
+    public int findBasement(String instructions) {
+        int position = 0;
+        int floor = 0;
+        char[] ins = instructions.toCharArray();
+
+        for (char i : ins) {
+            position++;
+
+            switch(i) {
+                case '(':
+                    floor++;
+                    break;
+                case ')':
+                    floor--;
+                    break;
+            }
+
+            if (floor == -1) {
+                break;
+            }
+        }
+
+        System.out.println("Day1NotQuiteList.findBasement: " + position);
+
+        return position;
     }
 }
--- src/test/java/com/geoff/aoc/_2015/Day1NotQuiteLispTest.java
+++ src/test/java/com/geoff/aoc/_2015/Day1NotQuiteLispTest.java
@@ -2,7 +2,6 @@ package com.geoff.aoc._2015;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,8 +14,77 @@ public class Day1NotQuiteLispTest {
     @Test
     void testFindFloor1() {
         String input = "(())";
-        int result = day1.findFloor(input);
 
-        assertEquals(0, result);
+        assertEquals(0, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor2() {
+        String input = "()()";
+        
+        assertEquals(0, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor3() {
+        String input = "(((";
+        
+        assertEquals(3, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor4() {
+        String input = "(()(()(";
+        
+        assertEquals(3, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor5() {
+        String input = "))(((((";
+        
+        assertEquals(3, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor6() {
+        String input = "())";
+        
+        assertEquals(-1, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor7() {
+        String input = "))(";
+        
+        assertEquals(-1, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor8() {
+        String input = ")))";
+        
+        assertEquals(-3, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindFloor9() {
+        String input = ")())())";
+        
+        assertEquals(-3, day1.findFloor(input));
+    }
+
+    @Test
+    void testFindBasement1() {
+        String input = ")";
+
+        assertEquals(1, day1.findBasement(input));
+    }
+
+    @Test
+    void testFindBasement2() {
+        String input = "()())";
+
+        assertEquals(5, day1.findBasement(input));
     }
 }
